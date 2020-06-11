6354bafdba2a785daa97ea1ac2c13e811ea400fe
Mikita Kukavenka <godinfrog@gmail.com>
2020-06-11 18:58:27 +0300

    changes

--- langs/old runic/from_slave_runic_to_old_runic.sql
+++ langs/old runic/from_slave_runic_to_old_runic.sql
@@ -1,18 +1,46 @@
--- from 17620
--- to 44670
+-- from 27510
+-- to 27520
 
 insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
-(17620, 44670, 0, 'ə','o'),
-(17620, 44670, 0, 'ă','a'),
-(17620, 44670, 0, 'jo','au̯'),
-(17620, 44670, 0, 'ja','ai̯'),
-(17620, 44670, 0, 'j$','ai̯'),
-(17620, 44670, 0, 'x','gʰ'),
-(17620, 44670, 0, 'y','ek'),
-(17620, 44670, 0, 'g̑h','gʰ'),
-(17620, 44670, 0, 'gh','gʰ'),
-(17620, 44670, 0, 'g̑','gʰ'),
-(17620, 44670, 0, 'bh','zb'),
-(17620, 44670, 0, 'ho','gʰo'),
-(17620, 44670, 0, 'ha','gʰa'),
-(17620, 44670, 0, 'h','gʰu');
+(27510, 27520, 0, 'ə','o'),
+(27510, 27520, 0, 'ă','a'),
+(27510, 27520, 0, 'jo','au̯'),
+(27510, 27520, 0, 'ja','ai̯'),
+(27510, 27520, 0, 'j$','ai̯'),
+(27510, 27520, 0, 'x','gʰ'),
+(27510, 27520, 0, 'y','ek'),
+(27510, 27520, 0, 'g̑h','gʰ'),
+(27510, 27520, 0, 'gh','gʰ'),
+(27510, 27520, 0, 'g̑','gʰ'),
+(27510, 27520, 0, 'bh','zb'),
+(27510, 27520, 0, 'ho','gʰo'),
+(27510, 27520, 0, 'ha','gʰa'),
+(27510, 27520, 0, 'h','gʰu');
+
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(27510, 27520, 0, 'zs', 'z'),
+(27510, 27520, 0, 'ds', 'dz'),
+(27510, 27520, 0, 'gs', 'ks'),
+(27510, 27520, 0, 'gt', 'gd'),
+(27510, 27520, 0, 'kn', 'gn'),
+(27510, 27520, 0, 'lt', 'ld'),
+(27510, 27520, 0, 'pn', 'bn'),
+(27510, 27520, 0, 'sg', 'sk'),
+(27510, 27520, 0, 'sn', 'zn'),
+(27510, 27520, 0, 'θn', 'ðn'),
+(27510, 27520, 0, 'kgʰ', 'gʰ'),
+(27510, 27520, 0, 'pg', 'bg'),
+(27510, 27520, 0, 'gvgʰ', 'gv');
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(27510, 27520, 0, 'pg', 'bg');
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(27510, 27520, 0, 'k̑k', 'gʰ');
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(27510, 27520, 0, 'gvg', 'gv');
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(27510, 27520, 0, 'gvh', 'gv');
