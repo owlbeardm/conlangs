438a0759a61ede66ea17ab1743d23c2de3625159
Mikita Kukavenka <godinfrog@gmail.com>
2020-07-28 20:08:49 +0300

    more staff

---
+++ langs/old westcoast/from_east_empire_to_old_westcoast.sql
@@ -0,0 +1,33 @@
+-- from 107540
+-- to 118770
+-- (?<=.)(tl)(?=.)
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+-- ai
+-- au
+-- oi
+-- (eu)
+-- (ui)
+
+iː > i
+ɪ > e
+eː > e
+aː > a
+oː > o
+uː > u
+ʊ > o
+pp > p
+tt > t
+kk > k
+VdV > ø
+VgV > ø
+VpV > b
+VtV > d
+VkV > g
+aw > o
+au > o
+^ce > sje
+ce > se
+ci > si
+m$ > ø
+-- (107540, 118770, 0, 'gʷʰ', 'gʷ');
