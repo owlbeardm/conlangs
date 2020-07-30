9f00b84e07efe2c085132c4c281a1e84defa8228
Mikita Kukavenka <godinfrog@gmail.com>
2020-07-30 17:57:14 +0300

    more stuff

---
+++ langs/east empire/from_old_empire_to_east_empire.sql
@@ -0,0 +1,9 @@
+-- from 118770
+-- to 118780
+-- (?<=.)(tl)(?=.)
+
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+-- (118770, 118780, 0, '(p)(?=.*kʷ)', 'kʷ'),
+(118770, 118780, 0, 'gn', 'ɣn');
+
+-- (118770, 118780, 0, 'gʷʰ', 'gʷ');
---
+++ langs/westcoast/notes.md
@@ -0,0 +1,5 @@
+https://www.staff.ncl.ac.uk/i.e.mackenzie/allophonr.htm
+
+http://mertsahinoglu.com/research/spanish-for-speakers-of-latin/
+
+https://www.spanishetym.com/phonology
