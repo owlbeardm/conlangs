71f8cb2b45330a285471fbe6c94dd86f00233e4d
Mikita Kukavenka <godinfrog@gmail.com>
2020-07-31 18:13:05 +0300

    all changes

---
+++ langs/westcoast/from_east_empire_to_old_westcoast.sql
@@ -0,0 +1,5 @@
+-- from 118780
+-- to 118790
+insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+(118790, 118800, 0, 'ʦ', 'θ');
+-- (118780, 118790, 0, 'gʷʰ', 'gʷ');
