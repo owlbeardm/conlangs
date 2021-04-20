8d5ec0ea9efdf414d4b085d023b99c1c457755d7
Mikita Kukavenka <godinfrog@gmail.com>
2021-04-20 19:11:26 +0300

    added nerlendic

--- langs/add_langs.sql
+++ langs/add_langs.sql
@@ -1,9 +1,7 @@
 insert into language_tbl(id, name) values
 (9820, 'Titan'),
-(9830, 'English'),
-(27510, 'SlaveRunic'),
-(107540, 'ProtoHuman'),
-(118770, 'OldEmpire'),
-(118780, 'EastEmpire'),
-(118790, 'OldWestCoast'),
-(118800, 'WestCoast');
+(9830, 'SlaveRunic'),
+(9850, 'ProtoHuman'),
+(9860, 'Queran'),
+(9870, 'OldNerlendic'),
+(9880, 'Nerlendic');
