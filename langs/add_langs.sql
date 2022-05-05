d2a915cd708f8bda6121e56ca1cafe928c14af2b
Mikita Kukavenka <godinfrog@gmail.com>
2022-05-05 18:55:43 +0300

    changes to langs

--- langs/add_langs.sql
+++ langs/add_langs.sql
@@ -1,7 +1,7 @@
 insert into language_tbl(id, name) values
 (9820, 'Titan'),
-(9830, 'SlaveRunic'),
-(9850, 'ProtoHuman'),
+(9830, 'Slave Runic'),
+(9850, 'Proto-Human'),
 (9860, 'Queran'),
 (9870, 'OldNerlendic'),
 (9880, 'Nerlendic');
--- langs/westcoast/from_old_westcoast_to_westcoast.sql
+++ langs/westcoast/from_old_westcoast_to_westcoast.sql
@@ -1,43 +1,43 @@
 -- from 118780
--- to 9870
-insert into evolve_law_tbl(lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
+-- to 10050
+insert into sound_change_tbl(id, lang_from_id, lang_to_id, priority, sound_regex_from, sound_to) values
 
 -- [ ] Metathesis s + /j/ > /i̯s/ (2.5.2.2(6)).
-(9870, 9880, 0, 'sj', 'js'),
+(nextval('pk_sequence'),10050, 10060, 0, 'sj', 'js'),
 
 -- [ ] Merger of final /i/ and /e/ in /e/ (2.4.3.2).
-(9870, 9880, 0, 'ie', 'e'),
-(9870, 9880, 0, 'ei', 'e'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ie', 'e'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ei', 'e'),
 
 -- [ ] Loss of final /e/ after ungrouped dental and alveolar consonants (except /t/) (2.4.3.2).
-(9870, 9880, 0, 'θe$', 'θ'),
-(9870, 9880, 0, 'ðe$', 'ð'),
-(9870, 9880, 0, 'de$', 'd'),
-(9870, 9880, 0, 'ne$', 'n'),
-(9870, 9880, 0, 'ʦe$', 'ʦ'),
-(9870, 9880, 0, 'ʣe$', 'ʣ'),
-(9870, 9880, 0, 'se$', 's'),
-(9870, 9880, 0, 'ze$', 'z'),
-(9870, 9880, 0, 're$', 'r'),
-(9870, 9880, 0, 'ɾe$', 'ɾ'),
-(9870, 9880, 0, 'le$', 'l'),
+(nextval('pk_sequence'),10050, 10060, 0, 'θe$', 'θ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ðe$', 'ð'),
+(nextval('pk_sequence'),10050, 10060, 0, 'de$', 'd'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ne$', 'n'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʦe$', 'ʦ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʣe$', 'ʣ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'se$', 's'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ze$', 'z'),
+(nextval('pk_sequence'),10050, 10060, 0, 're$', 'r'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ɾe$', 'ɾ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'le$', 'l'),
 
 -- [ ] OSp. *b* and *β* merge in /b/ (2.6. I).
-(9870, 9880, 0, 'β', 'b'),
+(nextval('pk_sequence'),10050, 10060, 0, 'β', 'b'),
 
 -- [ ] Syllable-final *b* > /u̯/ (2.5.5 (end)).
-(9870, 9880, 0, 'b$', 'u'),
+(nextval('pk_sequence'),10050, 10060, 0, 'b$', 'u'),
 
 -- [ ] Deaffrication of *ʦ* > /ʂ/ and *ʣ* > /ʐ/ t.1 (2.6.2).
-(9870, 9880, 0, 'ʦ', 'ʂ'),
-(9870, 9880, 0, 'ʣ', 'ʐ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʦ', 'ʂ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʣ', 'ʐ'),
 
 -- [ ] Devoicing of voiced sibilants and merger with voiceless counterparts: *ʐ* > /ʂ/, *z* > /s/, *ʒ* > /ʃ/ (2.6.2).
-(9870, 9880, 0, 'ʐ', 'ʂ'),
-(9870, 9880, 0, 'z', 's'),
-(9870, 9880, 0, 'ʒ', 'ʃ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʐ', 'ʂ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'z', 's'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʒ', 'ʃ'),
 
 -- [ ] Shift of locus *ʂ* > /θ/ (except in much of Andalusia and all America) and *ʃ* > /x/ or /h/ (2.6.2). 
-(9870, 9880, 0, 'ʂ', 'θ'),
-(9870, 9880, 0, 'ʃ', 'h');
+(nextval('pk_sequence'),10050, 10060, 0, 'ʂ', 'θ'),
+(nextval('pk_sequence'),10050, 10060, 0, 'ʃ', 'h');
 
